476c0ac8c2905707c674ef6b6c58e6c338136220
baogia0912 <51979397+baogia0912@users.noreply.github.com>
2021-08-18 21:29:09 +0700

    test database connection

--- src/server.js
+++ src/server.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+}).then(() => console.log('DB connected!')).catch(err => console.error(err))
+
+
 app.get('/', (req, res) => {
 	res.status(200).send('')
 })
