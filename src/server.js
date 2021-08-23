41568b877dd61884095e170b9fb5bf55e6bc203f
Trần Trọng Vỹ <s3764425@rmit.edu.vn>
2021-08-23 22:38:24 +0700

    delete dashboard due to corrupt

--- src/server.js
+++ src/server.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require("mongoose");
+const BodyParser = require("body-parser");
+
+Mongoose.connect("mongodb://localhost/vntalking");
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+// Define REST API
+app.post("/patient", async (request, response) => {
+
+})
+
+
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/App.js')
 })
