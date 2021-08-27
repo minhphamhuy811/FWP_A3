3ea8e43e66ef40338e58ccde779db2a56d88d66d
Trần Trọng Vỹ <s3764425@rmit.edu.vn>
2021-08-27 12:53:00 +0700

    add the dashboard skeleton

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,10 +5,10 @@ import logobyt from '../assets/logo_byt.svg'
 
 export default function Dashboard() {
   return(
-    <div className="flex">
-    <div className="bg-primary min-h-screen max-w-full">
-      <div className="border-b border-primary px-8 shadow-2xl">
-        <div className="flex flex-col gap-y-2 py-4">
+    <div className="flex flex-col lg:flex-row">
+    <div className="bg-primary h-1/2 lg:min-h-screen flex-shrink md:flex-shrink-0">
+      <div className="border-b border-primary mx-6 shadow-2xl">
+        <div className="flex flex-col gap-y-5 py-4">
           <div className="flex justify-center gap-x-2">
           <img src={logochxhcnvn} className="h-14" alt=""/>
           <img src={logobyt} className="h-14 border-b border-transparent p-0.5 rounded-full bg-white" alt=""/>
@@ -19,28 +19,29 @@ export default function Dashboard() {
       <div className="min-h-screen" style={{backgroundColor: '#00142a'}}>
         <div className="flex flex-col">
           <button onClick="userToggle()"
-                  className="flex text-xl text-white font-bold items-center gap-x-2 py-4 px-8 border border-transparent hover:bg-black focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-              <path
-                d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"/>
+                  className="flex text-xl text-white font-bold items-center gap-x-2 py-4 px-6 border border-transparent hover:bg-black focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                    d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>
             </svg>
             Patients
           </button>
           <button onClick="branchToggle()"
-                  className="flex text-xl text-white font-bold items-center gap-x-2 py-4 px-8 border border-transparent hover:bg-black focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd"
-                    d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z"
-                    clipRule="evenodd"/>
+                  className="flex text-xl text-white font-bold items-center gap-x-2 py-4 px-6 border border-transparent hover:bg-black focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                    d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"/>
             </svg>
             Admin
           </button>
           <a href="/"
-             className="flex text-xl text-white font-bold items-center gap-x-2 py-4 px-8 border border-transparent hover:bg-black focus:outline-none">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd"
-                    d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                    clipRule="evenodd"/>
+             className="flex text-xl text-white font-bold items-center gap-x-2 py-4 px-6 border border-transparent hover:bg-black focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
             </svg>
             <div>Log Out</div>
           </a>
@@ -48,8 +49,15 @@ export default function Dashboard() {
       </div>
     </div>
 
-      <div></div>
-
+      <div id="patients" className="flex-shrink w-full overflow-hidden">
+        <div className="border-b border-transparent shadow-2xl">
+          <div className="flex justify-between items-center ">
+            <div className="py-12">
+              <div className="text-4xl font-bold mx-6">Patients</div>
+            </div>
+          </div>
+        </div>
+      </div>
 
   </div>
   )
