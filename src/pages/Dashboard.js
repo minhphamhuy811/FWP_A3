2498da60ced1d7b6da23227955f45ac3beb3c02a
tranguyen <s3823237@rmit.edu.vn>
2021-09-10 01:03:23 +0700

    remove export button

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,6 @@ import React from "react";
 import logochxhcnvn from "../assets/logo_chxhcnvn.svg";
 import logobyt from "../assets/logo_byt.svg";
 import PatientTable from "../components/PatientTable";
-import FileToDownload from "../lib/utils/data.csv";
 import { useSessionStorage } from '../middleware/UseStorage'
 import { Link } from "react-router-dom";
 
@@ -115,23 +114,6 @@ export default function Dashboard() {
           </div>
         </div>
         <div className="flex flex-col mt-4 px-4 gap-y-4">
-          <div className="flex justify-end">
-            <button className="flex gap-x-1 items-center border border-primary p-2 text-primary rounded-xl font-bold hover:bg-primary hover:text-white focus:outline-none focus:text-white focus:bg-black">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-              </svg>
-              <a href={FileToDownload} download="data.csv">
-                {" "}
-                EXPORT{" "}
-              </a>
-            </button>
-          </div>
           <PatientTable />
         </div>
       </div>
