7c91711d3b20700ecb249c12f82c09d5558a4d37
tranguyen <s3823237@rmit.edu.vn>
2021-09-11 11:36:19 +0700

    fix patient table

--- src/components/PatientTable.js
+++ src/components/PatientTable.js
@@ -42,7 +42,7 @@ export default function PatientTable() {
   return (
     <div className="flex flex-col container mx-auto">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="flex justify-end">
+        <div className="flex justify-end pr-8 py-3">
           <button className="flex gap-x-1 items-center border border-primary p-2 text-primary rounded-xl font-bold hover:bg-primary hover:text-white focus:outline-none focus:text-white focus:bg-black">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -61,100 +61,125 @@ export default function PatientTable() {
         </div>
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <thead className="bg-gray-50">
-              <div className="grid grid-cols-5 px-6 py-3 items-center  border-black sm:rounded-lg">
+            <div
+              className="grid grid-cols-2 py-5 items-center bordered "
+              style={{ backgroundColor: "#00142a" }}
+            >
+              <div className="grid grid-cols-5 pl-4">
                 <div
                   scope="col"
-                  className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  className="text-center text-xs font-medium text-white uppercase tracking-wider"
                 >
                   ID
                 </div>
                 <div
                   scope="col"
-                  className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  className="text-center text-xs font-medium text-white uppercase tracking-wider"
                 >
                   HỌ TÊN
                 </div>
                 <div
                   scope="col"
-                  className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  className="text-center text-xs font-medium text-white uppercase tracking-wider"
                 >
                   GIỚI TÍNH
                 </div>
                 <div
                   scope="col"
-                  className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  className="text-center text-xs font-medium text-white uppercase tracking-wider"
                 >
                   QUẬN
                 </div>
                 <div
                   scope="col"
-                  className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  className="text-center text-xs font-medium text-white uppercase tracking-wider"
                 >
                   PHƯỜNG
                 </div>
               </div>
-              {patientList.map((row) => (
+              <div className="grid grid-cols-1">
                 <div
-                  key={row._id}
-                  className="collapse min-w-full border rounded-box border-base-300 collapse-arrow"
+                  scope="col"
+                  className="text-center text-xs font-medium text-white uppercase tracking-wider"
                 >
-                  <input type="checkbox" />
-                  <div className="collapse-title">
-                    <div className="grid grid-cols-5 px-6 py-3 items-center  border-black sm:rounded-lg">
+                  TÌNH TRẠNG
+                </div>
+              </div>
+            </div>
+            {patientList.map((row) => (
+              <div
+                key={row._id}
+                className="collapse min-w-full border border-base-300 collapse-arrow"
+              >
+                <input type="checkbox" />
+                <div className="collapse-title">
+                  <div className="grid grid-cols-2 py-1 items-center border-black sm:rounded-lg">
+                    <div className="grid grid-cols-5">
                       <div
                         scope="col"
-                        className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        className="text-center text-xs font-medium text-black uppercase tracking-wider"
                       >
                         {row.id}
                       </div>
                       <div
                         scope="col"
-                        className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        className="text-center text-xs font-medium text-black uppercase tracking-wider"
                       >
                         {row.fullName}
                       </div>
                       <div
                         scope="col"
-                        className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        className="text-center text-xs font-medium text-black uppercase tracking-wider"
                       >
                         {row.gender}
                       </div>
                       <div
                         scope="col"
-                        className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        className="text-center text-xs font-medium text-black uppercase tracking-wider"
                       >
                         {row.district}
                       </div>
                       <div
                         scope="col"
-                        className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        className="text-center text-xs font-medium text-black uppercase tracking-wider"
                       >
                         {row.ward}
                       </div>
                     </div>
-                  </div>
-                  <div className="collapse-content">
-                    <p>
-                      <span className="inline-grid grid-cols-4 gap-x-10 text-sm text-left font-medium text-gray-500">
-                        <span>Địa chỉ: {row.address}</span>
-                        <span>Quốc gia: {row.country}</span>
-                        <span>Email: {row.email}</span>
-                        <span>SĐT: {row.phoneNumber}</span>
-                      </span>
-                    </p>
-                    <p>
-                      <span className="inline-grid grid-cols-1 text-sm text-left font-medium text-gray-500">
+                    <div className="grid grid-cols-1">
+                      <div
+                        scope="col"
+                        className="text-center text-xs font-medium text-black uppercase tracking-wider"
+                      >
                         <span>
                           Triệu chứng hiện hữu: {checkSymptom(row).join(", ")}
                         </span>
-                        <span>Nhiệt độ: {row.fever}</span>
-                      </span>
-                    </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              ))}
-            </thead>
+                <div className="collapse-content">
+                  <div className="grid grid-cols-2 px-3">
+                    <div className="grid grid-cols-2">
+                      <div className="text-sm text-left font-medium text-gray-500">
+                        <p>Địa chỉ: {row.address}</p>
+                        <p>Quốc gia: {row.country}</p>
+                      </div>
+                      <div className="text-sm text-left font-medium text-gray-500">
+                        <p>Email: {row.email}</p>
+                        <p>SĐT: {row.phoneNumber}</p>
+                      </div>
+                    </div>
+                    <div className="text-sm text-left font-medium ml-10 text-gray-500">
+                      <div className="inline-grid grid-cols-1 text-sm text-left font-medium text-gray-500">
+                        <p>Triệu chứng: {checkSymptom(row).join(", ")}</p>
+                        <p>Nhiệt độ: {row.fever}{" "}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
